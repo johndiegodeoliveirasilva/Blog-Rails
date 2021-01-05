5920dbb3b04a6911775394a331c9bb142c300ecb
johndiego <johnoliveirasilva9@gmail.com>
2021-01-05 16:45:41 -0200

    Remove Unnescessary Function

--- app/javascript/controllers/sideNav_controller.js
+++ app/javascript/controllers/sideNav_controller.js
@@ -3,15 +3,12 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ["output"]
 
-  say_hello() {
-  this.outputTarget.textContent = "Hello World!"
-  }
-
   openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
+  
   closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";

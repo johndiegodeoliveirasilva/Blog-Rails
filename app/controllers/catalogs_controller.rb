49aa656257130a6ed7b074c9e9eefe16b6fad200
John Diego <johnoliveirasilva9@gmail.com>
2021-01-07 22:01:03 -0300

    Create  controler new, create

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,5 +2,26 @@ class CatalogsController < ApplicationController
   def index
     @catalogs = Catalog.all
   end
-  
+
+  def show
+  end
+
+  def new
+    @catalog = Catalog.new
+  end
+
+  def create
+    @catalog = Catalog.new(params[:id])
+    
+    if @catalog.save
+      redirect_to @catalog
+    else
+      render :new
+    end
+  end
+
+  private
+    def catalog_params
+      params.require(:catalog).permit(:title, :body)
+    end
 end

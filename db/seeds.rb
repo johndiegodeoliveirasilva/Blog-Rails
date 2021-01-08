7556e8d72fe45bc0070d8ecf268584ee705ffffd
John Diego <johnoliveirasilva9@gmail.com>
2021-01-07 21:33:53 -0300

    Generate seeds catalog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+8.times do |_|
+Catalog.create!(
+  title: Faker::Book.title,
+  body: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false))
+  puts 'Creating "#{_}'
+end

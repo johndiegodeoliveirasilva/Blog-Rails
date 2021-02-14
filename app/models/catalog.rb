9825e9615cf1bb89368442277a8f22ba97475ff9
John Diego <johnoliveirasilva9@gmail.com>
2021-02-14 16:06:08 -0200

    Add Referentes to Catalog

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,4 +2,5 @@ class Catalog < ApplicationRecord
   validates :title, presence: true, length: {minimum:3 }
   validates :body, presence: true, length: {minimum:10}
   has_one_attached :avatar
+  has_many :ingredients
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_015415) do
+ActiveRecord::Schema.define(version: 2021_02_14_171816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_015415) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "id_ingredient"
   end
 
   create_table "ingredients", force: :cascade do |t|

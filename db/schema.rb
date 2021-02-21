15b0f9cb74e0c1e39c9c2ea44dc2f876c5353fae
John Diego <johnoliveirasilva9@gmail.com>
2021-02-21 13:08:10 -0300

    Sdsd

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_02_16_234530) do
-=======
 ActiveRecord::Schema.define(version: 2021_02_14_171816) do
->>>>>>> References
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
